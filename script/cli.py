962ac344037a6ad36961a5ed057eb52a458e5839
gosha <george.kontsevik@gmail.com>
2022-03-10 23:54:57 +0300

    fixed some small things

--- script/cli.py
+++ script/cli.py
@@ -17,13 +17,12 @@ def main():
     parser_data_info = parser.add_argument_group(title='data_info')
     parser_data_info.add_argument('-year', nargs='?', const=0, default=2023, type=int, help='Год прогнозирования')
     parser_data_info.add_argument('-city-id', nargs='?', const=0, default=1, type=int, help='Город')
-    parser_data_info.add_argument('--set_population', nargs='?', const=0, default=0, type=int,
+    parser_data_info.add_argument('--set-population', nargs='?', const=0, default=0, type=int,
                                   help='Задать число жителей в год прогнозирования (суммарно по городу)')
     parser_data_info.add_argument('--path', nargs='?', const=0, default='./Output_data/', type=str,
                                   help='Папка сохранения')
-    parser_data_info.add_argument('--update_in_db', default=False,
+    parser_data_info.add_argument('--update-in-db', default=True,
                                   help='Update buildings.resident_number in database')
-
 
     args = parser.parse_args()
     main_file.main(args)
