7efca7d6d9e2a82c526af3ccc836b2f35633a8d3
gosha <george.kontsevik@gmail.com>
2022-03-10 14:20:25 +0300

    fixed cli

--- script/cli.py
+++ script/cli.py
@@ -16,7 +16,7 @@ def main():
     # Group_2 "Data args"
     parser_data_info = parser.add_argument_group(title='data_info')
     parser_data_info.add_argument('-year', nargs='?', const=0, default=2023, type=int, help='Год прогнозирования')
-    parser_data_info.add_argument('-city_id', nargs='?', const=0, default=1, type=int, help='Город')
+    parser_data_info.add_argument('-city-id', nargs='?', const=0, default=1, type=int, help='Город')
     parser_data_info.add_argument('--set_population', nargs='?', const=0, default=0, type=int,
                                   help='Задать число жителей в год прогнозирования (суммарно по городу)')
     parser_data_info.add_argument('--path', nargs='?', const=0, default='./Output_data/', type=str,
