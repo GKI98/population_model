cc905a2486c6f3297d5b30d722088c8f6d4f4dcc
gosha <george.kontsevik@gmail.com>
2022-03-13 22:43:17 +0300

    fixed .loc in city_population_forecast

--- script/city_population_forecast.py
+++ script/city_population_forecast.py
@@ -109,9 +109,11 @@ def main(path):
         #    value = (df[2020 + i] * df3['coef'])
 
         # Это с пропусками в исходном датафрейме
-        df[2020 + i][1] = df.iloc[1].median()
         print('PERVOE', df[2020 + i][1])
         print('VTOROE', df.iloc[1].median(), '\n')
+
+        df[2020 + i][1] = df.iloc[1].median()
+
         value = (df[2020 + i][:] * df3['coef'][:])
         value = iteround.saferound(value.values, 0)
         df.insert(loc=col_num, column=column, value=value, allow_duplicates=True)
