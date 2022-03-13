0baf93987d0918a6be22c954cac4d713aaedfb4b
gosha <george.kontsevik@gmail.com>
2022-03-14 00:23:47 +0300

    fixed .loc -> assign in balance_houses

--- script/balance_houses.py
+++ script/balance_houses.py
@@ -42,7 +42,7 @@ def balance_houses_population(houses_df_upd, path) -> None:
     mun_age_sex_df = pd.read_csv(f'{path}/mun_age_sex_df.csv')
     mun_list = set(houses_df_upd['municipality_id'])
     houses_df_upd.assign(**{'citizens_reg_bal': houses_df_upd['prob_population']})
-    print(houses_df_upd.haed())
+    print(houses_df_upd.head())
     # Минимальное значение, до которого может сокращаться населения в доме при балансировке, кол-во человек
     balancing_min = 5
     # Точность балансировки, кол-во человек
