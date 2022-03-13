85e1b8163796d9b188f77ea0344fbbe40e8a6a0c
gosha <george.kontsevik@gmail.com>
2022-03-13 21:24:52 +0300

    fixed by sokol comments

--- script/changes_coef.py
+++ script/changes_coef.py
@@ -6,8 +6,8 @@ import pandas as pd
 def main(year=2020, path=''):
     # path = '/home/gk/code/tmppycharm/ifmo_1/script/data/'
 
-    changes_forecast = pd.read_csv(f'{path}changes_forecast.csv')
-    city_forecast_years_age_ratio = pd.read_csv(f'{path}city_forecast_years_age_ratio.csv')
+    changes_forecast = pd.read_csv(f'{path}/changes_forecast.csv')
+    city_forecast_years_age_ratio = pd.read_csv(f'{path}/city_forecast_years_age_ratio.csv')
 
     # Изменение в прогнозируемой численности в сравнении с 2019 годом (отношение к численности в 2019 по возрастам)
     coef_ages = changes_forecast[f'{year}']
@@ -15,7 +15,7 @@ def main(year=2020, path=''):
     # Состав населения в % в прогнозируемом году
     year_ratio = city_forecast_years_age_ratio[f'{year}']
 
-    city_population_forecast = pd.read_csv(f'{path}city_population_forecast.csv').drop(columns=['Unnamed: 0'])
+    city_population_forecast = pd.read_csv(f'{path}/city_population_forecast.csv').drop(columns=['Unnamed: 0'])
     population_sum = city_population_forecast.sum()
 
     change_coef = population_sum[f'{year}'] / population_sum[f'{2019}']
