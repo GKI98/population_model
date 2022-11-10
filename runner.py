0c90ee5be7eedb9ecac1fb5155e62bbaf3e91538
George <george.kontsevik@gmail.com>
2022-11-10 14:47:25 +0300

    should be stable version

--- runner.py
+++ runner.py
@@ -1,10 +1,12 @@
 import os
+import time
 
-scenarios = ['pos','mod','neg']
+scenarios = ['mod','pos','neg']
 cities = [2,5]
 
 for city in cities:
     for year in range(2022, 2031, 1):
         for scenario in scenarios:
             print(f'\n year: {year}, scenario: {scenario} \n')
-            os.system(f'python3 cli.py --year={year} --scenario={scenario} --city-id={city}')
+            os.system(f'python3 cli.py --year={year} --scenario={scenario} --city-id={city} -save=loc')
+            time.sleep(5)
