7f1aca135588217a71a9e9df3c7b4e1b8469116a
George <george.kontsevik@gmail.com>
2022-11-10 14:48:58 +0300

    small fix

--- runner.py
+++ runner.py
@@ -8,5 +8,5 @@ for city in cities:
     for year in range(2022, 2031, 1):
         for scenario in scenarios:
             print(f'\n year: {year}, scenario: {scenario} \n')
-            os.system(f'python3 cli.py --year={year} --scenario={scenario} --city-id={city} -save=loc')
+            os.system(f'python3 cli.py --year={year} --scenario={scenario} --city-id={city}')
             time.sleep(5)
